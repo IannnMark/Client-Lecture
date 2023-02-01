bda7e5420fcd4afd0b6379928716cc4cb50fc0d1
ianmark <ianmark>
2023-02-01 08:07:46 +0700

    commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import React from 'react';
+
+import Nav from './Nav';
+
+
+const App = () => (
+
+  <div className="container pb-5">
+
+      <Nav />
+
+      <br />
+
+      <h1>MERN CRUD</h1>
+
+  </div>
+
+);
+
+
+
+export default App;
